271bf3dad0c1df16515d611cf5d7a27a7ceabc93
OnX <13263359914@163.com>
2024-03-25 20:50:32 +0800

    a new fontend

--- PureBoard/frontend/src/components/HeaderBar.jsx
+++ PureBoard/frontend/src/components/HeaderBar.jsx
@@ -4,11 +4,11 @@ import "./comp.css"
 const HeaderBar = () => {
   return (
     <div className='flex justify-between items-center'>
-      <div className='p-6 font-bold'>
+      <div className='p-6 font-bold Hcolor'>
         PureBoard
       </div>
-      <div className='p-4 text-gray-400 hover:text-gray-200 transition cursor-pointer'>
-        Menu
+      <div className='p-4 text-gray-400 hover:text-gray-200 transition cursor-pointer Hcolor'>
+        About
       </div>
     </div>
   );
--- PureBoard/frontend/src/views/MainView.jsx
+++ PureBoard/frontend/src/views/MainView.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 const MainView = () => {
 	return (
-		<div className="p-6 flex justify-center flex-col w-full h-full">
-			<div className="font-bold text-5xl">New Board</div>
-			<p className="mt-4 text-1xl">Think for a new board for pad</p>
-			<p className="mt-2 text-1xl">Free to use</p>
-			<div className="mt-3 p-4 rounded-2xl bg-sky-400 hover:bg-sky-800 transition cursor-pointer w-40">
-				<a
-					href="https://zhiyiyi.lanzoul.com/ieb9R1s6k9vg"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Download App
-				</a>
+		<div className="p-6 flex justify-center items-center flex-col md:flex-row w-full h-full">
+			<div className="md:w-1/2">
+				<div className="font-bold text-5xl Mcolor">New Board</div>
+				<p className="mt-4 text-1xl Mcolor">Think for a new board for pad</p>
+				<p className="mt-2 text-1xl Mcolor">Free to use</p>
+				<p className="mt-2 md-2"> </p>
+				<div className="mt-3 p-4 gradient-button transition cursor-pointer w-60">
+					<a
+						href="https://zhiyiyi.lanzoul.com/ieb9R1s6k9vg"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Download App
+					</a>
+				</div>
+			</div>
+			<div className="md:w-1/2 mt-custom">
+				<img src="../src/imgs/show.png" alt="Your Image" />
 			</div>
 		</div>
 	);
